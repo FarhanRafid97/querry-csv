77a73c9ff240af35823ba35c36dd8e7a4c223a95
farhan rafid syauqi <farhanrafid97@gmail.com>
2025-11-21 10:40:09 +0700

    add rename save

--- src/components/modules/export-csv.tsx
+++ src/components/modules/export-csv.tsx
@@ -1,13 +1,28 @@
+import { FileIcon, MagicWandIcon } from '@radix-ui/react-icons';
+import { useRef, useState } from 'react';
 import { CSVLink } from 'react-csv';
 import { Button } from '../ui/button';
-import { useState } from 'react';
-import { MagicWandIcon } from '@radix-ui/react-icons';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogWrapperContent
+} from '@/components/ui/dialog';
+import { Input } from '../ui/input';
 
 const ExportCsv = ({ data, headers }: { data: object[]; headers: { label: string; key: string }[] }) => {
   const [isExporting, setIsExporting] = useState(false);
-
+  const [fileName, setFileName] = useState('');
+  const [open, setOpen] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ref = useRef<any>(null);
   const handleClick = () => {
     setIsExporting(true);
+    setOpen(false);
     setTimeout(() => {
       setIsExporting(false);
     }, 5000);
@@ -15,18 +30,46 @@ const ExportCsv = ({ data, headers }: { data: object[]; headers: { label: string
 
   return (
     <>
-      {isExporting ? (
-        <Button variant="outline" onClick={handleClick} disabled={isExporting}>
-          Delay Export
-        </Button>
-      ) : (
-        <CSVLink data={data} headers={headers}>
-          <Button variant="outline" onClick={handleClick} className="cursor-pointer">
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" disabled={isExporting}>
             <MagicWandIcon className="w-2 h-2" />
             Export
           </Button>
-        </CSVLink>
-      )}
+        </DialogTrigger>
+        <DialogContent
+          onCloseAutoFocus={() => {
+            // reset()
+            setFileName('');
+          }}
+        >
+          <DialogHeader>
+            <DialogTitle>Export CSV</DialogTitle>
+          </DialogHeader>
+          <DialogWrapperContent>
+            <Input
+              type="text"
+              placeholder="Enter file name"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleClick();
+                  ref.current.click();
+                }
+              }}
+              value={fileName}
+              onChange={(e) => setFileName(e.target.value)}
+            />
+          </DialogWrapperContent>
+          <DialogFooter className="sm:justify-end">
+            <CSVLink ref={ref} data={data} headers={headers} filename={fileName}>
+              <Button variant="outline" onClick={handleClick} className="cursor-pointer">
+                <FileIcon className="w-2 h-2" />
+                Save
+              </Button>
+            </CSVLink>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>{' '}
     </>
   );
 };
